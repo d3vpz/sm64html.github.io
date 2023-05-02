052fba703cfc077f8774b9dce21dd43e07591cae
Devpooldotmsi <114014131+Devpooldotmsi@users.noreply.github.com>
2023-05-02 15:49:45 +0000

    converting amp geo to js

---
+++ data/dependencies/functions.js
@@ -0,0 +1,17 @@
+function GEO_SHADOW(t, c, o) {
+    return t, c, o;
+}
+
+function GEO_OPEN_NODE() {
+    return false;
+}
+
+function GEO_SCALE(u, s) {
+    return u, s;
+}
+
+function GEO_ANIMATED_PART(l, x, y, z, dl) {
+    return l, x, y, z, dl;
+}
+
+export { functions }
---
+++ data/dependencies/types.js
@@ -0,0 +1,6 @@
+let layers = [
+    LAYER_OPAQUE = null,
+    LAYER_ALPHA = null,
+]
+
+export { types }
